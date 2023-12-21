ca679b433acadb49e973c5ab5bcf913c8459f5e5
Icomanman <fred_barabs@yahoo.com>
2023-12-22 12:36:08 +1300

    chore: added sh script to start venv.

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 
+import random
 import gradio as gr
+
+
+def random_response(message, history):
+    return random.choice(["Yes", "No"])
+
+
+def main():
+    print("> This is Gradio.")
+    demo = gr.ChatInterface(random_response)
+    demo.launch()
+    return
+
+
+if __name__ == '__main__':
+    main()
